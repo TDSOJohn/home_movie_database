0bcaaffa8ee893310edad4129e081e43d4e859db
TDSOJohn <giovanni.basso@icloud.com>
2020-05-29 21:32:35 +0200

    d

--- add_query.php
+++ add_query.php
@@ -2,24 +2,14 @@
     echo date('Y-m-d H:i:s');
     $title = $_GET['name'];
     $key = '74a10e01';
-    $url = 'http://www.omdbapi.com/t='.$title.'&apikey='.$key;
+    $url = 'http://www.omdbapi.com/t='.$title.'&apikey='.$key.'/';
     echo "\n $url";
 
-    function get_web_page($i_url) {
-        $options = array(
-            CURLOPT_RETURNTRANSFER => true,   // return web page
-            CURLOPT_HEADER         => false,  // don't return headers
-            CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-            CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-            CURLOPT_ENCODING       => "",     // handle compressed
-            CURLOPT_USERAGENT      => "test", // name of client
-            CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-            CURLOPT_TIMEOUT        => 120,    // time-out on response
-        );
-
-        $ch = curl_init($i_url);
-        curl_setopt_array($ch, $options);
+    function get_web_page($i_url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "$url");
+        curl_setopt($ch, CURLOPT_HEADER, 0);
 
         $content  = curl_exec($ch);
 
