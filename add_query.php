6696deebd57ae9b50bab1cc73715c1a12530fc70
TDSOJohn <giovanni.basso@icloud.com>
2020-05-29 21:03:35 +0200

    updating add_query.php

--- add_query.php
+++ add_query.php
@@ -2,7 +2,7 @@
     echo date('Y-m-d H:i:s');
     $title = $_GET['name'];
     $key = '74a10e01';
-    $url = 'http://www.omdbapi.com/t='.$title.'&apikey='.$key;
+    $url = 'http://www.omdbapi.com/t='.$title.'&apikey='.$key+'/';
     echo "\n $url";
 
     function get_web_page($i_url) {
