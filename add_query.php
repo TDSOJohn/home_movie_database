b7763cd2c1c412af8aacda549b78e939d1e8b281
TDSOJohn <giovanni.basso@icloud.com>
2020-05-30 00:56:44 +0200

    added json decoder in add_query.php

--- add_query.php
+++ add_query.php
@@ -1,9 +1,7 @@
 <?php
-    echo date('Y-m-d H:i:s');
     $title = $_GET['name'];
     $key = '74a10e01';
     $url = 'http://www.omdbapi.com/?'.'apikey='.$key.'&t='.$title;
-    echo "\n $url";
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, "$url");
@@ -11,5 +9,6 @@
     $response = curl_exec($ch);
     echo "\n $response";
     curl_close($ch);
-    echo "hi everyone!";
+    var_dump(json_decode($response));
+    var_dump(json_decode($response, true));
 ?>
