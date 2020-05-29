b4a3a7919e12e9db7e37b7186a97d06160450ede
TDSOJohn <giovanni.basso@icloud.com>
2020-05-29 22:39:30 +0200

    fixing add_query.php

--- add_query.php
+++ add_query.php
@@ -4,22 +4,10 @@
     $key = '74a10e01';
     $url = 'http://www.omdbapi.com/t='.$title.'&apikey='.$key;
     echo "\n $url";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "$url");
 
-    function get_web_page($i_url)
-    {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "$url");
-
-        $content  = curl_exec($ch);
-        echo "\n $content";
-        curl_close($ch);
-
-        return $content;
-    }
-
-    $response = get_web_page($url);
-    echo("\n $response");
-    $resArr = array();
-    $resArr = json_decode($response);
-    echo "<pre>"; print_r($resArr); echo "</pre>";
+    $response = curl_exec($ch);
+    echo "\n $response";
+    curl_close($ch);
 ?>
