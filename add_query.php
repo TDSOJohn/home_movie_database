a27f7af25b39c8fa50035036061569f983f5cf30
TDSOJohn <giovanni.basso@icloud.com>
2020-05-30 14:20:45 +0200

    structuring add_query.php code

--- add_query.php
+++ add_query.php
@@ -1,17 +1,30 @@
 <?php
     ini_set('display_errors', '1');
 
-    $title = $_GET['name'];
-    $key = '74a10e01';
-    $url = 'http://www.omdbapi.com/?'.'apikey='.$key.'&t='.$title;
+#   returns the correct query url, complete with api key
+    function return_query_url($in_string)
+    {
+        $key = '74a10e01';
+        $url = 'http://www.omdbapi.com/?'.'apikey='.$key.'&t='.$in_string;
+        return $url;
+    }
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "$url");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    $response = curl_exec($ch);
-    curl_close($ch);
+    function curl_call($in_url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "$in_url");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $out_data = curl_exec($ch);
+        curl_close($ch);
+
+        return $out_data;
+    }
+
+    $title = $_GET['name'];
+    $url = return_query_url($title);
+    $json_movie_info = curl_call($url);
 
     var_dump(json_decode($response));
-?>
 
-# load HTML from a file
+    $file = 'movies_database.json';
+?>
