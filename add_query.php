a64751f94928deff99e0a52a555d0dadb3d2b93e
TDSOJohn <giovanni.basso@icloud.com>
2020-05-30 19:15:28 +0200

    updating add_query.php

--- add_query.php
+++ add_query.php
@@ -1,3 +1,5 @@
+#   add a string which is movie title without whitespaces, punctuation
+
 <?php
     ini_set('display_errors', '1');
 
@@ -16,11 +18,13 @@
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "$in_url");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $out_data = curl_exec($ch);
+        $cod_content = curl_exec($ch);
         curl_close($ch);
-        echo "$out_data\n";
 
-        return $out_data;
+        $dec_content = json_decode($cod_content);
+        var_dump("$dec_content\n");
+
+        return $dec_content;
     }
 
 #   reads a file, attempts to decode with json_decode, returns array of object(stdClass) of FALSE
@@ -50,15 +54,30 @@
         fclose($handle);
     }
 
+    function search_json_by_name($dec_content, $search, $name)
+    {
+        foreach($dec_content->Title as $item)
+        {
+            echo "$item";
+            if($item == $search)
+            {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
     $title = $_GET['name'];
     $url = return_omdb_query_url($title);
-    $json_movie_info = curl_call($url);
-    var_dump(json_decode($json_movie_info));
+    $curl_json_data = curl_call($url);
+    var_dump($curl_json_data));
 
     $file = 'movies_database.json';
+    $movies_db = read_from_json($file);
 
-    write_json_to_file($file, $json_movie_info);
-
-    $movies_array = read_from_json($file);
-    var_dump($movies_array);
+    if(!search_json_by_name($movies_db, "Blade Runner", "Title"))
+    {
+        write_json_to_file($file, $curl_json_data);
+    }
+    var_dump($movies_db);
 ?>
