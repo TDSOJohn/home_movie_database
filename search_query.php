4c4f7ee4bd036b4fd2987dee15386c60360362ce
TDSOJohn <giovanni.basso@icloud.com>
2020-06-01 18:13:31 +0200

    updating add_query.php and search_query.php

--- search_query.php
+++ search_query.php
@@ -22,6 +22,7 @@
     $movies_db = read_from_json($db_filename);
 
     echo $movies_db;
+    $movies_db = json_decode($movies_db);
     $dec_movies_db = json_decode($movies_db);
     var_dump($dec_movies_db);
 
