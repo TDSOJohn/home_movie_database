8cac26f20d9aea7116734228d464100ebc768c16
TDSOJohn <giovanni.basso@icloud.com>
2020-06-17 20:56:47 +0200

    adding javascript

--- javascript/json_interface.js
+++ javascript/json_interface.js
@@ -2,10 +2,11 @@ function callPhp() {
     var title = document.getElementById('title').value;
 
     var xmlhttp = new XMLHttpRequest();
-    alert("starting shit");
+    alert(title);
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
+            alert(myObj[1]);
             document.getElementById("demo").innerHTML = myObj[2];
         }
     };
